bbd905b71bad050007eabbfb8d833778af9b62c4
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2021-09-23 20:23:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var menu_list_array = ["Veg Margherita Pizza", "Neapolitan Pizza", "Chicago Pizza", "New York-Style Pizza", "Sicilian Pizza", "Greek Pizza", "California Pizza", "Detroit Pizza", "St. Louis Pizza"];
+
+function getmenu() {
+    var htmldata;
+    menu_list_array.sort();
+    for (var i = 0; i < menu_list_array.length; i++) {
+        htmldata = htmldata + menu_list_array[i] < br > ;
+    }
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item() {
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.sort();
+    htmldata = ""
+    for (var i = 0; i < menu_list_array.length; i++) {
+        htmldata = htmldata + imgtags + menu_list_array[i] < br > ;
+    }
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
+
+function add_top() {
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    add_item();
+}
